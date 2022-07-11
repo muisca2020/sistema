d43d6016f1ff3336e8b2f5f9e6b2d9a1f72abd39
Leonardo Sanchez <leonsanc@gmail.com>
2022-07-10 23:25:33 -0500

    adición al archivo js para categoria de todas las funciones para manupular el formulario y tabla de categoria

--- vistas/scripts/categoria.js
+++ vistas/scripts/categoria.js
@@ -1,15 +1,75 @@
+// Este archivo contine funciones javascript que mediante jQuery manipulan el DOM y sus propiedades y valores
+
 var tabla;
 
-// Funcion init
+// Funcion init que inicializa
 function init() 
 {
-
+    mostarform(false);
+    listar();
 }
 
-// Funcion limpiar
+// Funcion limpiar que limpia el contenido de los campos
 function limpiar() 
 {
+    $("idcategoria").val("");
     $("#nombre").val("");
     $("#descripcion").val("");
 }
+
+// Funcion mostarform que dependienteo del valor del parámetro, muestra el div del listado y esconde el formulario o viceversa
+function mostarform(flag) 
+{
+    limpiar();
+    if (flag)
+    {
+        $("#listadoregistros").hide();
+        $("#formularioregistros").show();
+        $("btnGuardar").prop("disabled", false);
+    }
+    else
+    {
+        $("#listadoregistros").show();
+        $("#formularioregistros").hide();
+    }
+}
+
+// Funcion cancelarform permite ocultar un formulario que ya no se va a usar
+function cancelarform()
+{
+    limpiar();
+    mostarform(false);
+}
+
+// Función listar para enviar un valor mediante ajax a categoria.php
+function listar()
+{
+    tabla = $("#tbllistado").dataTable(
+        {
+            "aProcessing": true, // Para activar el procesamiento del datatable
+            "aServerSide": true, // Para indicar que la paginación y el filtadro se hacen del lado del servidor
+            dom: 'Bfrtip', // Para definir los elementos de control de la tabla
+            buttons: [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdf'
+            ],
+            "ajax":
+                {
+                    url: '../ajax/categoria.php?op=listar',
+                    type: 'GET',
+                    dataType: 'json',
+                    error: function(e){
+                        console.log(e.responseText);
+                    }
+                },
+            "bDestory": true, // Para
+            "iDisplayLenght": 5, // Para indicar de cada cuanto se realizará la paginación
+            "order": [[ 0, "desc" ]] // Para indicar el orden por la columna 0 en orden descendente
+        }
+    ).DataTable();
+
+}
+
 init();
